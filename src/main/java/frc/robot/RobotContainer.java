cd1d0ebc237294f100887966c44c5e54f8537829
Charan Nellore <charan.nellore11@gmail.com>
2025-11-15 11:28:49 -0600

    CN - Untested base tank drive code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,9 +15,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
 
+public class RobotContainer {
+
+  private DriveTrainSubsystem drive;
+  // The robot's subsystems and commands are defined here...
+  
   // Replace with CommandPS4Controller or CommandJoystick if needed
 
   // Initialize DriveTrainSubsystem
@@ -30,7 +33,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
-
+    drive = new DriveTrainSubsystem();
     // Instantiate controller and drive
     driverController = new CommandXboxController(ControllerConstants.kDriverControllerPort);
     configureBindings();
@@ -38,7 +41,10 @@ public class RobotContainer {
     // Set a default command for the DriveTrainSubsystem. This is where you supply your joystick
     // inputs as the speed and rotation for arcadeDrive. Keep in mind that you MUST use a lambda expression, as
     // this ensures the program is checking for the joystick to have moved periodically (every 20 milliseconds).
-
+    drive.setDefaultCommand(new DriveTrainCommand(
+            () -> -driverController.getLeftY(),
+            () -> driverController.getRightX(),
+            drive));  
   }
 
   
---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class DriveTrainSubsystem extends SubsystemBase {
+  /** Creates a new DriveTrainSubsystem. */
+
+  // Declaring the four drive motors
+  private CANSparkMax frontLeft;
+  private CANSparkMax frontRight;
+  private CANSparkMax backLeft;
+  private CANSparkMax backRight;
+  
+  private DifferentialDrive drive;
+
+  public DriveTrainSubsystem() {
+    // Instantiates the four drive motors
+    frontLeft = new CANSparkMax(Constants.DriveConstants.FRONT_LEFT_ID, MotorType.kBrushless);
+    frontRight = new CANSparkMax(Constants.DriveConstants.FRONT_RIGHT_ID, MotorType.kBrushless);
+    backLeft = new CANSparkMax(Constants.DriveConstants.BACK_LEFT_ID, MotorType.kBrushless);
+    backRight = new CANSparkMax(Constants.DriveConstants.BACK_RIGHT_ID, MotorType.kBrushless);
+
+    drive new DifferentialDrive(frontLeft, frontRight);
+
+    backLeft.follow(frontLeft);
+    backRight.follow(frontRight);
+
+    frontLeft.setInverted(true);
+    backLeft.setInverted(true);
+
+    configureMotor(frontLeft);
+    configureMotor(frontRight);
+    configureMotor(backLeft);
+    configureMotor(backRight);
+   }
+
+   public void arcadeDrive(double speed, double theta) {
+    drive.arcadeDrive(speed, theta);
+   }
+
+   private void configureMotor(CANSparkMax motor) {
+    motor.restoreFactoryDefaults();
+    motor.setSmartCurrentLimit(Constants.DriveConstants.DRIVE_CURRENT_LIMIT);
+    motor.setIdleMode(IdleMode.kBrake);
+    motor.burnFlash();
+   }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
