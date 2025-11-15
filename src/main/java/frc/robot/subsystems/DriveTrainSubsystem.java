7ef0f8058ef8673e08f19f9a3b256897e3dd0d75
prakritimathur2010-prog <prakriti.mathur.2010@gmail.com>
2025-11-15 11:28:48 -0600

    PM - Untested completed tank drive code

---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DriveTrainSubsystem extends SubsystemBase {
+  /** Creates a new DriveTrainSubsystem. */
+
+  // Declaring the four drive motos
+  private CANSparkMax frontLeft;
+  private CANSparkMax frontRight;
+  private CANSparkMax backLeft;
+  private CANSparkMax backRight;
+
+  private DifferentialDrive drive;
+
+  public DriveTrainSubsystem() {
+
+    frontLeft = new CANSparkMax(Constants.DriveConstants.FRONT_LEFT_ID, MotorType.kBrushless);
+    frontRight = new CANSparkMax(Constants.DriveConstants.FRONT_RIGHT_ID, MotorType.kBrushless);
+    frontRight = new CANSparkMax(Constants.DriveConstants.BACK_LEFT_ID, MotorType.kBrushless);
+    frontRight = new CANSparkMax(Constants.DriveConstants.BACK_RIGHT_ID, MotorType.kBrushless);
+
+    drive = new DifferentialDrive(frontLeft, frontRight);
+    
+    backLeft.follow(frontLeft);
+    backRight.follow(frontRight);
+    
+    frontLeft.setInverted(true);
+    backLeft.setInverted(true);
+
+    configuremotors(frontLeft);
+    configuremotors(frontRight); 
+    configuremotors(backLeft);
+    configuremotors(backRight);
+  }
+
+  public void arcadeDrive(double speed, double theta) {
+    drive.arcadeDrive(speed, theta);
+    
+  }
+
+  private void configuremotors(CANSparkMax motor){
+    motor.restoreFactoryDefaults();
+    motor.setSmartCurrentLimit(Constants.DriveConstants.DRIVE_CURRENT_LIMIT);
+    motor.setIdleMode(IdleMode.kBrake);
+    motor.burnFlash();
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
